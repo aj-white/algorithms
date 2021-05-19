aa2afe0f31386040e8d3351fb7865c40f98c0c14
Andrew White <white-aj@outlook.com>
2021-05-19 21:14:59 +0100

    add linear_search

---
+++ linear_search.py
@@ -0,0 +1,23 @@
+from typing import List, Union
+
+
+def linear_search(items: List, target: Union[int, str]) -> int:
+    """Returns the index position of the target if found, else None"""
+    for index, value in enumerate(items):
+        if items[index] == target:
+            return index
+    return None
+
+
+def verify(index: int) -> str:
+    if index is not None:
+        print(f"Target found at index {index}")
+    else:
+        print("Target is not in list")
+
+
+numbers = [*range(1, 20)]
+search_result = linear_search(numbers, 8)
+verify(search_result)
+search_result = linear_search(numbers, 21)
+verify(search_result)
